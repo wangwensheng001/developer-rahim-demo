3265768f7fbeca83ac5f0817532f970a9b93cdec
flutterdev-rahim <largeitlink2@gmail.com>
2022-07-22 18:53:14 +0600

    a

--- lib/screens/git2.dart
+++ lib/screens/git2.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:cupertino_icons/cupertino_icons.dart';
+import 'package:cupertino_icons/cupertino_icons.dart'; //////////;
+
 class Git2 extends StatefulWidget {
-
-
   @override
   State<Git2> createState() => _Git2State();
 }
@@ -11,7 +10,7 @@ class _Git2State extends State<Git2> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('hel'),
+      child: Text('hel222222222222222222'),
     );
   }
 }
